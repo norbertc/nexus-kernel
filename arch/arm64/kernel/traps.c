8206f96523b0284a92363780fbc6db48b3405696
Norbert Copones <nrbrt.cpns@gmail.com>
2017-02-11 01:49:22 +0800

    arm64: avoid returning from bad mode

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -411,26 +411,14 @@ asmlinkage long do_ni_syscall(struct pt_regs *regs)
  */
 asmlinkage void bad_mode(struct pt_regs *regs, int reason, unsigned int esr)
 {
-	siginfo_t info;
-	void __user *pc = (void __user *)instruction_pointer(regs);
 	console_verbose();
 
 	pr_crit("Bad mode in in %s handler detected, code 0x%08x\n",
 		handler[reason], esr);
-	__show_regs(regs);
 
-	info.si_signo = SIGILL;
-	info.si_errno = 0;
-	info.si_code  = ILL_ILLOPC;
-	info.si_addr  = pc;
-
-	if (esr >> ESR_EL1_EC_SHIFT == ESR_EL1_EC_SERROR) {
-		pr_crit("System error detected. ESR.ISS = %08x\n",
-			esr & 0xffffff);
-		arm64_check_cache_ecc(NULL);
-	}
-
-	arm64_notify_die("Oops - bad mode", regs, &info, 0);
+	die("Oops - bad mode", regs, 0);
+	local_irq_disable();
+	panic("bad mode");
 }
 
 /*
