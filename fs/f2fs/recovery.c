4c7d6f52abfbed3b16df28df4a02b7cc797e2edc
Jaegeuk Kim <jaegeuk@kernel.org>
2017-08-12 21:33:23 -0700

    f2fs: check hot_data for roll-forward recovery
    commit 125c9fb1ccb53eb2ea9380df40f3c743f3fb2fed upstream.
    
    We need to check HOT_DATA to truncate any previous data block when doing
    roll-forward recovery.
    
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- fs/f2fs/recovery.c
+++ fs/f2fs/recovery.c
@@ -206,7 +206,7 @@ static void check_index_in_prev_nodes(struct f2fs_sb_info *sbi,
 		return;
 
 	/* Get the previous summary */
-	for (i = CURSEG_WARM_DATA; i <= CURSEG_COLD_DATA; i++) {
+	for (i = CURSEG_HOT_DATA; i <= CURSEG_COLD_DATA; i++) {
 		struct curseg_info *curseg = CURSEG_I(sbi, i);
 		if (curseg->segno == segno) {
 			sum = curseg->sum_blk->entries[blkoff];
